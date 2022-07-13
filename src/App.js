3456dd1024c0d6173a3de3127b3435f202a4e1c6
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-13 08:05:21 +0300

    styles added..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Images from "./Images";
+import "./styles.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src="" alt="selected" className="selected" />
+      </div>
+      <div className="imgContainer">
+        {Images.map((img, index)=>(
+          <img key={index} src={img} alt="vista" />
+        )
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Images.js
@@ -0,0 +1,7 @@
+export default [
+    "https://github.com/akif-onder/react-image-gallery/blob/master/public/images/photo1.jpg",
+    "https://github.com/akif-onder/react-image-gallery/blob/master/public/images/photo2.jpg",
+    "https://github.com/akif-onder/react-image-gallery/blob/master/public/images/photo3.jpg",
+    "https://github.com/akif-onder/react-image-gallery/blob/master/public/images/photo4.jpg",
+    "https://github.com/akif-onder/react-image-gallery/blob/master/public/images/photo5.jpg"
+]
